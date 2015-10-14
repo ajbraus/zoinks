5738608363dcd08c22b2a9c837f6afa63220bcef
Adam Braus <adambraus@Six-Persimmons.local>
2015-10-13 17:51:29 -0700

    blh"

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,22 +8,83 @@ angular.module('myApp.controllers', [])
          loginService.logout();
       };
 
-      $scope.toggleSidenav = function() {
-         console.log('blah')
-         if ($scope.showSidenav) {
-            $scope.showSidenav = false;   
-         } else {
-            $scope.showSidenav = true;
-         }
-      }
+      // $scope.toggleSidenav = function() {
+      //    if ($scope.showSidenav) {
+      //       $scope.showSidenav = false;   
+      //    } else {
+      //       $scope.showSidenav = true;
+      //    }
+      // }
    }])
 
    .controller('HomeCtrl', ['$scope', 'syncData', function($scope, syncData) {
       syncData('syncedValue').$bind($scope, 'syncedValue');
    }])
 
-   .controller('HappeningShowCtrl', ['$scope', 'syncData', '$routeParams', 'Happening', function($scope, syncData, Happening) {
+   .controller('HappeningsIndexCtrl', ['$scope', 'Happening', function($scope, Happening) {
+      $scope.happenings = Happening.all();
+   }])
+
+   .controller('HappeningShowCtrl', ['$scope', 'syncData', '$routeParams', 'Happening', function($scope, syncData, $routeParams, Happening) {
       $scope.happening = Happening.get($routeParams.id);
+
+      // CARPOOLS
+      $scope.toggleNewCarpool = function() {
+         if ($scope.newCarpool == true) {
+            $scope.newCarpool = false;
+         } else {
+            $scope.newCarpool = true;
+         }
+      }
+      $scope.addCarpool = function() {
+         // carpool.driver = currentUser;
+         $scope.happening.carpools.unshift($scope.carpool)
+         $scope.newCarpool = false;
+         $scope.carpool = {};
+      }
+
+      // REQUIREMENTS
+      $scope.toggleNewRequirement = function() {
+         if ($scope.newRequirement == true) {
+            $scope.newRequirement = false;
+         } else {
+            $scope.newRequirement = true;
+         }
+      }
+      $scope.addRequirement = function() {
+         $scope.happening.requirements.unshift($scope.requirement)
+         $scope.newRequirement = false;
+         $scope.requirement = {};
+      }
+
+      // TODOS
+      $scope.toggleNewTodo = function() {
+         if ($scope.newTodo == true) {
+            $scope.newTodo = false;
+         } else {
+            $scope.newTodo = true;
+         }
+      }
+      $scope.addTodo = function() {
+         $scope.happening.todos.unshift($scope.todo)
+         $scope.newTodo = false;
+         $scope.todo = {};
+      }
+
+      // INVITES
+      $scope.toggleNewInvite = function() {
+         if ($scope.newInvite == true) {
+            $scope.newInvite = false;
+         } else {
+            $scope.newInvite = true;
+         }
+      }
+      $scope.addInvite = function() {
+         $scope.happening.invites.unshift($scope.invite)
+         $scope.newInvite = false;
+         $scope.invite = {};
+      }
+      
    }])
 
   .controller('ChatCtrl', ['$scope', 'syncData', function($scope, syncData) {
--- app/js/services.js
+++ app/js/services.js
@@ -6,39 +6,52 @@
    angular.module('myApp.services', ['myApp.service.login', 'myApp.service.firebase'])
 
     .service('Happening', [function() {
+      var users = [
+        { name: "Bob Daniels"},
+        { name: "Matthew Charley"},
+        { name: "Mary Sonja"}
+      ]
 
       var happenings = [{
         id: 0,
         title: "Zen Camping",
         location: "Camp Fort Doggity Dog",
+        startsAt: "6pm Oct 6",
+        endsAt: "5pm Oct 8",
+        happeningOn: "Oct 6-8",
         costInCents: 1000,
-        groupRequirements: [
+        todos: ["Bring a hat", "Bring water", "Get ready to have fun!"],
+        requirements: [
           { owner: "", body: "Bring a Tent", completedAt: new Date() },
-          { owner: "", body: "Bring a Tent", completedAt: new Date() },
-          { owner: "", body: "Bring a Tent", completedAt: new Date() }
+          { owner: "", body: "Bring a Stove", completedAt: new Date() },
+          { owner: "", body: "Bring a Paddle", completedAt: new Date() }
         ],
         carpools: [
           { name: "Toyota", driver: "", passengers: [], seats: 4 },
           { name: "Toyota", driver: "", passengers: [], seats: 3 }
         ],
         invites: ["invite1@test.com", "invite2@test.com", "invite3@test.com"],
-        rsvps: [users[0], users[1], user[2]]
+        rsvps: [users[0], users[1], users[2]]
       }, {
         id: 1,
         title: "Martian Landing Lookout",
         location: "Camp Fort Doggity Dog",
+        startsAt: "6pm Oct 6",
+        endsAt: "5pm Oct 8",
+        happeningOn: "Oct 6-8",
         costInCents: 1000,
-        groupRequirements: [
+        todos: ["Bring a hat", "Bring water", "Get ready to have fun!"],
+        requirements: [
           { owner: "", body: "Bring a Tent", completedAt: new Date() },
-          { owner: "", body: "Bring a Tent", completedAt: new Date() },
-          { owner: "", body: "Bring a Tent", completedAt: new Date() }
+          { owner: "", body: "Bring a Paddle", completedAt: new Date() },
+          { owner: "", body: "Bring a Stove", completedAt: new Date() }
         ],
         carpools: [
-          { name: "Toyota", driver: "", passengers: [], seats: 4 },
-          { name: "Toyota", driver: "", passengers: [], seats: 3 }
+          { name: "Benji", driver: "Bob", passengers: [users[1]], seats: 4 },
+          { name: "The Blue Pill", driver: "Sonja", passengers: [users[2], users[0]], seats: 3 }
         ],
         invites: ["invite1@test.com", "invite2@test.com", "invite3@test.com"],
-        rsvps: [users[0], users[1], user[2]]
+        rsvps: [users[0], users[1], users[2]]
       }];
 
       return {
